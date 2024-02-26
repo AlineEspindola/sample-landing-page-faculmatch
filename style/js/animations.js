c0aa04199a5849426df61537a6c10978de65b9bc
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2024-02-26 14:18:09 -0300

    feat: Adiciona animações

--- style/js/animations.js
+++ style/js/animations.js
@@ -14,6 +14,33 @@ const observador = new IntersectionObserver( (elementos) => {
                     case 'subtextoDemanda':
                         animarfadeInUp1400ms(elementos[i])
                         break
+                    case 'tituloProjeto':
+                        animarfadeInUp(elementos[i])
+                        break
+                    case 'textoProjeto':
+                        animarfadeInUp700ms(elementos[i])
+                        break
+                    case 'imagemProjeto':
+                        animarfadeInUp1400ms(elementos[i])
+                        break
+                    case 'tituloEquipe':
+                        animarfadeInUp(elementos[i])
+                        break
+                    case 'textoEquipe':
+                        animarfadeInUp700ms(elementos[i])
+                        break
+                    case 'card1':
+                        animarfadeInUp1400ms(elementos[i])
+                        break
+                    case 'card2':
+                        animarfadeInUp2100ms(elementos[i])
+                        break
+                    case 'card3':
+                        animarfadeInUp2800ms(elementos[i])
+                        break
+                    case 'card4':
+                        animarfadeInUp3500ms(elementos[i])
+                        break
                 }
             }else{
                 switch(entrada.id){
@@ -21,10 +48,37 @@ const observador = new IntersectionObserver( (elementos) => {
                         animarfadeInUp(elementos[i])
                         break
                     case 'subtextoDemanda':
-                        animarfadeInUp700ms(elementos[i])
+                        animarfadeInUp(elementos[i])
                         break
                     case 'desenhoLivros':
-                        animarfadeInLeft1400ms(elementos[i])
+                        animarfadeInLeft(elementos[i])
+                        break
+                    case 'tituloProjeto':
+                        animarfadeInUp(elementos[i])
+                        break
+                    case 'textoProjeto':
+                        animarfadeInUp(elementos[i])
+                        break
+                    case 'imagemProjeto':
+                        animarfadeInUp(elementos[i])
+                        break
+                    case 'tituloEquipe':
+                        animarfadeInUp(elementos[i])
+                        break
+                    case 'textoEquipe':
+                        animarfadeInUp(elementos[i])
+                        break
+                    case 'card1':
+                        animarfadeInUp(elementos[i])
+                        break
+                    case 'card2':
+                        animarfadeInUp(elementos[i])
+                        break
+                    case 'card3':
+                        animarfadeInUp(elementos[i])
+                        break
+                    case 'card4':
+                        animarfadeInUp(elementos[i])
                         break
                 }
             }
@@ -60,6 +114,24 @@ function animarfadeInUp1400ms(elemento){
     elemento.target.classList.add('visivel')
     elemento.target.classList.add('animate__animated')
     elemento.target.classList.add('fadeInUp-1400ms')
+}
+
+function animarfadeInUp2100ms(elemento){
+  elemento.target.classList.add('visivel')
+  elemento.target.classList.add('animate__animated')
+  elemento.target.classList.add('fadeInUp-2100ms')
+}
+
+function animarfadeInUp2800ms(elemento){
+  elemento.target.classList.add('visivel')
+  elemento.target.classList.add('animate__animated')
+  elemento.target.classList.add('fadeInUp-2800ms')
+}
+
+function animarfadeInUp3500ms(elemento){
+  elemento.target.classList.add('visivel')
+  elemento.target.classList.add('animate__animated')
+  elemento.target.classList.add('fadeInUp-3500ms')
 }
 
 const secoes = document.querySelectorAll('.escondido')
